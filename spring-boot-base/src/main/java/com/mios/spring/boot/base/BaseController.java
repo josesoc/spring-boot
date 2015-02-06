5d63cac1262de31578704d6034ef75c553cdfed8
josesoc <josesocialnetworks@gmail.com>
2015-02-06 14:03:51 +0100

    logging support

--- spring-boot-base/src/main/java/com/mios/spring/boot/base/BaseController.java
+++ spring-boot-base/src/main/java/com/mios/spring/boot/base/BaseController.java
@@ -1,5 +1,7 @@
 package com.mios.spring.boot.base;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +15,20 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class BaseController {
-
+ 
+	static Logger logger = LoggerFactory.getLogger(BaseController.class);
+	
     @RequestMapping("/")
     public String home(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        logger.info("home controller mapping");
         return "welcome";
     }
     
     @RequestMapping("/list")
     public String list(Model model) {
         model.addAttribute("list", "data list");
+        logger.info("list controller mapping");
         return "list";
     }
 }
