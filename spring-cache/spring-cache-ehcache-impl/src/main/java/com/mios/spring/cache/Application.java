382865b7dfa6739160c874ea3619befbb6e40c2e
josesoc <josesocialnetworks@gmail.com>
2015-05-21 17:01:20 +0200

    spring-cache-ehcache-impl remove annotation profile

--- spring-cache/spring-cache-ehcache-impl/src/main/java/com/mios/spring/cache/Application.java
+++ spring-cache/spring-cache-ehcache-impl/src/main/java/com/mios/spring/cache/Application.java
@@ -10,7 +10,6 @@ import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 @EnableCaching
-//@Profile("ehcache")
 public class Application {
 
 	public static void main(String[] args) {
